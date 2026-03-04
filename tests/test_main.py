ff31b850b42f570fbbfe2989b123565bad3e8759
Lionel JOURDHIER <lio.jourdhier@gmail.com>
2026-03-04 08:51:40 +0100

    Maj test_main.py

--- tests/test_main.py
+++ tests/test_main.py
@@ -1,36 +1,20 @@
-import subprocess
-import sys
-from pathlib import Path
+from unittest.mock import patch
 
-# On force l'ajout de la racine du projet au chemin de recherche
-# pour être sûr que 'import main' fonctionne peu importe où on lance le test
-sys.path.insert(0, str(Path(__file__).parent.parent))
+import pandas as pd
 
-from app.main import main
+from app.main import get_values_from_csv
 
 
-def test_main_output(capsys):
-    """Vérifie que la fonction main affiche le message attendu."""
-    main()
+def test_get_values_from_csv():
+    """Garantit 100% de couverture sur la fonction de lecture."""
+    fake_data = {"col1": [1, 2], "col2": [3, 4]}
+    fake_df = pd.DataFrame(fake_data)
 
-    # Capture la sortie du terminal
-    captured = capsys.readouterr()
+    with patch("pandas.read_csv") as mock_read:
+        mock_read.return_value = fake_df
 
-    # .strip() permet d'ignorer les espaces ou sauts de ligne inutiles
-    assert captured.out.strip() == "Hello from mlobs2!"
+        result = get_values_from_csv("dummy_path.csv")
 
-
-def test_script_execution():
-    """Teste l'exécution du fichier main.py en tant que script (CLI)."""
-    # On trouve la racine du projet (le dossier qui contient 'tests')
-    # .resolve() transforme le chemin en chemin absolu sans '..'
-    root_dir = Path(__file__).parent.parent.resolve()
-    script_path = root_dir / "app" / "main.py"
-
-    # On lance le fichier
-    result = subprocess.run(
-        [sys.executable, str(script_path)], capture_output=True, text=True
-    )
-    # On vérifie le résultat
-    assert result.returncode == 0
-    assert "Hello from mlobs2!" in result.stdout
+        assert isinstance(result, pd.DataFrame)
+        assert result.iloc[0, 0] == 1
+        mock_read.assert_called_once_with("dummy_path.csv")
