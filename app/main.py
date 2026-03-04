80798f02a0354fd21da4c000f847b1fda46d775c
Lionel JOURDHIER <lio.jourdhier@gmail.com>
2026-03-04 10:31:48 +0100

    Mise à jour pour debug

--- app/main.py
+++ app/main.py
@@ -1,6 +1,10 @@
+import os
+
 import pandas as pd
 
 from app.modules.mon_module import add, print_data, square, sub
+
+CURRENT_DIR = os.path.dirname(os.path.abspath(__file__))  # pragma: no cover
 
 
 def get_values_from_csv(filepath: str) -> pd.DataFrame:
@@ -12,11 +16,12 @@ def get_values_from_csv(filepath: str) -> pd.DataFrame:
     Returns:
        pd.DataFrame: Un DataFrame contenant les données extraites du CSV.
     """
+    print(f"DEBUG: Le répertoire actuel est : {os.getcwd()}")
     return pd.read_csv(filepath)
 
 
 if __name__ == "__main__":  # pragma: no cover
-    filepath = "app/moncsv.csv"
+    filepath = os.path.join(CURRENT_DIR, "moncsv.csv")
     df = get_values_from_csv(filepath)
     print_data(df)  # Affiche le DataFrame dans le terminal
     print(add(3, 5))  # Addition
